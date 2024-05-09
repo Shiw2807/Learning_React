6c8c3b3b6d72485112dd92bcf1461faddd245fdc
shiwanshi <shiwanshi2807@gmail.com>
2024-05-09 14:02:45 +0530

    Event handling- FunctionClick.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
+      <FunctionClick/>
     </div>
   );
 }
---
+++ src/components/FunctionClick.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function FunctionClick() {
+
+    function clickHandlder(){
+        console.log("Button Clicked")
+    }
+    
+  return (
+    <div>
+      {/* <button onClick={clickHandlder()}>Click</button> //Not a function definition so won't work.
+      <button onClick={()=>clickHandlder()}>CLick Me</button>  */}
+      <button onClick={clickHandlder}>Clickk</button>
+    </div>
+  )
+}
